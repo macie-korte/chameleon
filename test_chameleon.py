ce36334d959eed7e38840f6dcc0f9bd9367b51ba
Agam Dua <agam.dua@dealertrack.com>
2015-05-29 00:05:55 -0400

    Adding Py3 support and other goodies
    Python 3
    =======
    Python 3 support mainly required a change in how the data was passed
    around - cherrypy is smart enough to distinguish between bytes and
    chars (which is expected if it is providing Py3 support)
    
    This also updated the page handler to conditionally return JSON back
    dependent on content type. This logic *may* look brittle but it should
    work just fine.
    
    Other Stuff
    ========
    - runnable unittest files
    - requirements.txt!
    - bug where function name was incorrect (stop_mock_service -> stop)
    - PEP8 (especially 80char lines)
    - Some refactoring where DRY-ness could be added
    - simplejson was added, but not as a hard dependency

--- test_chameleon.py
+++ test_chameleon.py
@@ -1,7 +1,7 @@
 import unittest
 import chameleon
 import requests
-import simplejson as json
+
 
 class ChameleonTests(unittest.TestCase):
     def tearDown(self):
@@ -13,14 +13,14 @@ class ChameleonTests(unittest.TestCase):
         responds to queries as expected.
         Port defaults to 8080.
         """
-        settings_list = [{"url": "/path1", 
-                          "status_code": 200, 
-                          "content_type": "application/json", 
+        settings_list = [{"url": "/path1",
+                          "status_code": 200,
+                          "content_type": "application/json",
                           "content": '{"foo": "bar"}'}]
         chameleon.start(settings_list)
         response = requests.get('http://localhost:8080/path1')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content, '{"foo": "bar"}')
+        self.assertEqual(response.content, b'{"foo": "bar"}')
         self.assertEqual(response.json()['foo'], 'bar')  # ensure response decoded as JSON.
         self.assertEqual(response.headers['content-type'], 'application/json')
 
@@ -29,25 +29,30 @@ class ChameleonTests(unittest.TestCase):
         Multiple URLs can be served by the mock server at once.
         Custom ports other than 8080 also work.
         """
-        settings_list = [{"url": "/path1", 
-                          "status_code": 200, 
-                          "content_type": "application/json", 
+        settings_list = [{"url": "/path1",
+                          "status_code": 200,
+                          "content_type": "application/json",
                           "content": '{"foo": "bar"}'},
-                         {"url": "path2", 
-                          "status_code": 404, 
-                          "content_type": "text/html", 
+                         {"url": "path2",
+                          "status_code": 404,
+                          "content_type": "text/html",
                           "content": 'Page not found'}]
         chameleon.start(settings_list, port=8000)
 
         # test path1 - the successful response
         response = requests.get('http://localhost:8000/path1')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content, '{"foo": "bar"}')
+        self.assertEqual(response.content, b'{"foo": "bar"}')
         self.assertEqual(response.json()['foo'], 'bar')  # ensure response decoded as JSON.
         self.assertEqual(response.headers['content-type'], 'application/json')
 
         # test path2 - the page-not-found path
         response = requests.get('http://localhost:8000/path2')
         self.assertEqual(response.status_code, 404)
-        self.assertEqual(response.content, 'Page not found')
-        self.assertTrue(response.headers['content-type'].startswith('text/html'))
+        self.assertEqual(response.content, b'"Page not found"')
+        self.assertTrue(
+            response.headers['content-type'].startswith('text/html')
+        )
+
+if __name__ == '__main__':
+    unittest.main()
